51d247ce7240e3414e4c21b7582a6dd14701c836
Stefan Sandor <stefansandor26@gmail.com>
2023-07-27 20:39:39 +0300

    Added a UI to keep the score

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-function getComputerChoice () 
-{
-    const choices = ['rock', "paper", "scissors"];
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 
-function playRound(playerSelection, computerSelection)
-{
+function playRound(playerSelection, computerSelection) {
     let result;
     if(playerSelection === 'rock')
     {
@@ -56,33 +54,106 @@ function playRound(playerSelection, computerSelection)
     return result;
 }
 
-function game()
-{
-    let scor = 0;
-    for (let i=0; i<5;i++)
-    {
-        const computerSelection = getComputerChoice();
-        console.log("Computer chose:", computerSelection);
-        const playerSelection = prompt("Type your weapon here", "rock/paper/scissors").toLowerCase();
-        if(playRound(playerSelection, computerSelection) === 'It\'s a draw!')
-        {
-            i--;
-        }
-        else if(playRound(playerSelection, computerSelection) === 'You win!')
-        {
-            scor++;
-        }
+let playerSelection
+    i=0
+    j=0
+    winnerDeclared = false;
+
+function updateScore() {
+    const punctajCalculator = document.createElement('div');
+    const punctajOm = document.createElement('div');
+    punctajCalculator.textContent = 'Computer Score: ' + i;
+    punctajOm.textContent = "Player Score: " + j;
+    const scorul = document.querySelector('#scor');
+    scorul.innerHTML = '';
+    scorul.appendChild(punctajCalculator);
+    scorul.appendChild(punctajOm);
+}
+
+function announceWinner() {
+    if (i >= 5) {
+        tabela.innerHTML = 'Computer wins the game!';
+        winnerDeclared = true;
+    } else if (j >= 5) {
+        tabela.innerHTML = 'You win the game!';
+        winnerDeclared = true;
     }
-    if(scor < 3)
-    {
-        console.log("Computer wins...by far");
-    }
-    else
-    {
-        console.log("You win...by far");
+
+    // If the winner is declared, disable the buttons
+    if (winnerDeclared) {
+        const buttons = document.querySelectorAll('button');
+        buttons.forEach((button) => {
+            button.disabled = true;
+        });
     }
 }
 
-console.log(game());
+function startGame() {
+    const container = document.querySelector('#container');
+    const button1 = document.createElement('button');
+    const button2 = document.createElement('button');
+    const button3 = document.createElement('button');
+    button1.textContent = 'Rock';
+    button2.textContent = 'Paper';
+    button3.textContent = 'Scissors';
+    const tabela = document.querySelector('#tabela');
 
+    // Add event listeners to the buttons
+    button1.addEventListener('click', () => {
+        if (!winnerDeclared) {
+            const computerSelection = getComputerChoice();
+            const rezultat = playRound('rock', computerSelection);
+            tabela.innerHTML = 'Computer chose: ' + computerSelection + '<br>' + rezultat;
+            console.log('Computer chose:', computerSelection);
+            console.log(playRound('rock', computerSelection));
 
+            if (rezultat == 'Computer wins!') {
+                i++;
+            } else if (rezultat == 'You win!') {
+                j++;
+            }
+
+            updateScore();
+            announceWinner();
+        }
+    });
+    button2.addEventListener('click', () => {
+        const computerSelection = getComputerChoice();
+        const rezultat = playRound('paper', computerSelection);
+        tabela.innerHTML = 'Computer chose: ' + computerSelection + '<br>' + rezultat;
+        console.log('Computer chose:', computerSelection);
+        console.log(playRound('paper', computerSelection));
+
+        if (rezultat == 'Computer wins!') {
+            i++;
+        } else if (rezultat == 'You win!') {
+            j++;
+        }
+
+        updateScore();
+        announceWinner();
+    });
+    button3.addEventListener('click', () => {
+        const computerSelection = getComputerChoice();
+        const rezultat = playRound('scissors', computerSelection);
+        tabela.innerHTML = 'Computer chose: ' + computerSelection + '<br>' + rezultat;
+        console.log('Computer chose:', computerSelection);
+        console.log(playRound('scissors', computerSelection));
+
+        if (rezultat == 'Computer wins!') {
+            i++;
+        } else if (rezultat == 'You win!') {
+            j++;
+        }
+
+        updateScore();
+        announceWinner();
+    });
+    
+    container.appendChild(button1);
+    container.appendChild(button2);
+    container.appendChild(button3);
+}
+
+// Call startGame() after the buttons are created
+startGame();
